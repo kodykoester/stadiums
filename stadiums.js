55c782483c8e7207dbb36f498dd24ffdd4be8336
kodykoester <105684121+kodykoester@users.noreply.github.com>
2024-07-30 12:20:49 -0500

    Add files via upload

---
+++ stadiums.js
@@ -0,0 +1,302 @@
+const airportDetails = {
+    "ARI": {
+        "name": "State Farm Stadium",
+        "location": "Glendale, Arizona",
+        "description": "Home of the Arizona Cardinals.",
+        "image": "/assets/ARI-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 63400,
+        "year_opened": 2006,
+        "playing_surface": "Grass"
+    },
+    "ATL": {
+        "name": "Mercedes-Benz Stadium",
+        "location": "Atlanta, Georgia",
+        "description": "Home of the Atlanta Falcons.",
+        "image": "./assets/ATL-In.jpg",
+        "other_teams": "Atlanta United FC",
+        "capacity": 71000,
+        "year_opened": 2017,
+        "playing_surface": "FieldTurf"
+    },
+    "BAL": {
+        "name": "M&T Bank Stadium",
+        "location": "Baltimore, Maryland",
+        "description": "Home of the Baltimore Ravens.",
+        "image": "./assets/BAL-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 70000,
+        "year_opened": 1998,
+        "playing_surface": "FieldTurf"
+    },
+    "BUF": {
+        "name": "Highmark Stadium",
+        "location": "Orchard Park, New York",
+        "description": "Home of the Buffalo Bills.",
+        "image": "./assets/BUF-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 71608,
+        "year_opened": 1973,
+        "playing_surface": "A-Turf Titan"
+    },
+    "CAR": {
+        "name": "Bank of America Stadium",
+        "location": "Charlotte, North Carolina",
+        "description": "Home of the Carolina Panthers.",
+        "image": "./assets/CAR-In.jpg",
+        "other_teams": "Charlotte FC",
+        "capacity": 74000,
+        "year_opened": 1996,
+        "playing_surface": "FieldTurf"
+    },
+    "CHI": {
+        "name": "Soldier Field",
+        "location": "Chicago, Illinois",
+        "description": "Home of the Chicago Bears.",
+        "image": "./assets/CHI-In.jpg",
+        "other_teams": "Chicago Fire FC",
+        "capacity": 61500,
+        "year_opened": 1924,
+        "playing_surface": "Kentucky Bluegrass"
+    },
+    "CIN": {
+        "name": "Paycor Stadium",
+        "location": "Cincinnati, Ohio",
+        "description": "Home of the Cincinnati Bengals.",
+        "image": "./assets/CIN-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 65515,
+        "year_opened": 2000,
+        "playing_surface": "UBU Sports Speed Series S5-M"
+    },
+    "CLE": {
+        "name": "Cleveland Browns Stadium",
+        "location": "Cleveland, Ohio",
+        "description": "Home of the Cleveland Browns.",
+        "image": "./assets/CLE-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 67531,
+        "year_opened": 1999,
+        "playing_surface": "Kentucky Bluegrass"
+    },
+    "DAL": {
+        "name": "AT&T Stadium",
+        "location": "Arlington, Texas",
+        "description": "Home of the Dallas Cowboys.",
+        "image": "./assets/DAL-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 80000,
+        "year_opened": 2009,
+        "playing_surface": "Matrix artificial turf"
+    },
+    "DEN": {
+        "name": "Empower Field at Mile High",
+        "location": "Denver, Colorado",
+        "description": "Home of the Denver Broncos.",
+        "image": "./assets/DEN-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 76125,
+        "year_opened": 2001,
+        "playing_surface": "Kentucky Bluegrass"
+    },
+    "DET": {
+        "name": "Ford Field",
+        "location": "Detroit, Michigan",
+        "description": "Home of the Detroit Lions.",
+        "image": "./assets/DET-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 65000,
+        "year_opened": 2002,
+        "playing_surface": "FieldTurf"
+    },
+    "GB": {
+        "name": "Lambeau Field",
+        "location": "Green Bay, Wisconsin",
+        "description": "Home of the Green Bay Packers.",
+        "image": "./assets/GB-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 81441,
+        "year_opened": 1957,
+        "playing_surface": "Grass (DD GrassMaster)"
+    },
+    "HOU": {
+        "name": "NRG Stadium",
+        "location": "Houston, Texas",
+        "description": "Home of the Houston Texans.",
+        "image": "./assets/HOU-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 72220,
+        "year_opened": 2002,
+        "playing_surface": "Hellas Matrix Helix"
+    },
+    "IND": {
+        "name": "Lucas Oil Stadium",
+        "location": "Indianapolis, Indiana",
+        "description": "Home of the Indianapolis Colts.",
+        "image": "./assets/IND-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 67000,
+        "year_opened": 2008,
+        "playing_surface": "FieldTurf"
+    },
+    "JAX": {
+        "name": "EverBank Stadium",
+        "location": "Jacksonville, Florida",
+        "description": "Home of the Jacksonville Jaguars.",
+        "image": "./assets/JAX-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 67164,
+        "year_opened": 1995,
+        "playing_surface": "Tifway 419 Bermuda Grass"
+    },
+    "KC": {
+        "name": "Arrowhead Stadium",
+        "location": "Kansas City, Missouri",
+        "description": "Home of the Kansas City Chiefs.",
+        "image": "./assets/KC-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 76416,
+        "year_opened": 1972,
+        "playing_surface": "Latitude 36 Bermuda Grass"
+    },
+    "LAC": {
+        "name": "SoFi Stadium",
+        "location": "Inglewood, California",
+        "description": "Home of the Los Angeles Chargers & the Los Angeles Rams.",
+        "image": "./assets/LAC-In.jpg",
+        "other_teams": "Los Angeles Rams",
+        "capacity": 70240,
+        "year_opened": 2020,
+        "playing_surface": "Artificial Turf"
+    },
+    "LV": {
+        "name": "Allegiant Stadium",
+        "location": "Paradise, Nevada",
+        "description": "Home of the Las Vegas Raiders.",
+        "image": "./assets/LV-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 65000,
+        "year_opened": 2020,
+        "playing_surface": "Bermuda Grass"
+    },
+    "MIA": {
+        "name": "Hard Rock Stadium",
+        "location": "Miami Gardens, Florida",
+        "description": "Home of the Miami Dolphins.",
+        "image": "./assets/MIA-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 65326,
+        "year_opened": 1987,
+        "playing_surface": "Platinum TE Paspalum"
+    },
+    "MIN": {
+        "name": "U.S. Bank Stadium",
+        "location": "Minneapolis, Minnesota",
+        "description": "Home of the Minnesota Vikings.",
+        "image": "./assets/MIN-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 66655,
+        "year_opened": 2016,
+        "playing_surface": "UBU Sports Speed Series S5-M"
+    },
+    "NE": {
+        "name": "Gillette Stadium",
+        "location": "Foxborough, Massachusetts",
+        "description": "Home of the New England Patriots.",
+        "image": "./assets/NE-In.jpg",
+        "other_teams": "New England Revolution",
+        "capacity": 65878,
+        "year_opened": 2002,
+        "playing_surface": "FieldTurf"
+    },
+    "NO": {
+        "name": "Caesars Superdome",
+        "location": "New Orleans, Louisiana",
+        "description": "Home of the New Orleans Saints.",
+        "image": "./assets/NO-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 73308,
+        "year_opened": 1975,
+        "playing_surface": "FieldTurf"
+    },
+    "NYG": {
+        "name": "MetLife Stadium",
+        "location": "East Rutherford, New Jersey",
+        "description": "Home of the New York Giants & the New York Jets.",
+        "image": "./assets/NYG-In.jpg",
+        "other_teams": "New York Jets",
+        "capacity": 82500,
+        "year_opened": 2010,
+        "playing_surface": "FieldTurf"
+    },
+    "PHI": {
+        "name": "Lincoln Financial Field",
+        "location": "Philadelphia, Pennsylvania",
+        "description": "Home of the Philadelphia Eagles.",
+        "image": "./assets/PHI-In.jpg",
+        "other_teams": "Temple Owls",
+        "capacity": 69596,
+        "year_opened": 2003,
+        "playing_surface": "Desso GrassMaster"
+    },
+    "PIT": {
+        "name": "Acrisure Stadium",
+        "location": "Pittsburgh, Pennsylvania",
+        "description": "Home of the Pittsburgh Steelers.",
+        "image": "./assets/PIT-In.jpg",
+        "other_teams": "Pitt Panthers",
+        "capacity": 68400,
+        "year_opened": 2001,
+        "playing_surface": "Kentucky Bluegrass"
+    },
+    "SF": {
+        "name": "Levi's Stadium",
+        "location": "Santa Clara, California",
+        "description": "Home of the San Francisco 49ers.",
+        "image": "./assets/SF-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 68500,
+        "year_opened": 2014,
+        "playing_surface": "Bermuda Bandera"
+    },
+    "SEA": {
+        "name": "Lumen Field",
+        "location": "Seattle, Washington",
+        "description": "Home of the Seattle Seahawks.",
+        "image": "./assets/SEA-In.jpg",
+        "other_teams": "Seattle Sounders FC",
+        "capacity": 68000,
+        "year_opened": 2002,
+        "playing_surface": "FieldTurf"
+    },
+    "TB": {
+        "name": "Raymond James Stadium",
+        "location": "Tampa, Florida",
+        "description": "Home of the Tampa Bay Buccaneers.",
+        "image": "./assets/TB-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 65890,
+        "year_opened": 1998,
+        "playing_surface": "Tifway 419 Bermuda Grass"
+    },
+    "TEN": {
+        "name": "Nissan Stadium",
+        "location": "Nashville, Tennessee",
+        "description": "Home of the Tennessee Titans.",
+        "image": "./assets/TEN-In.jpg",
+        "other_teams": "Nashville SC",
+        "capacity": 69143,
+        "year_opened": 1999,
+        "playing_surface": "Tifway 419 Bermuda Grass"
+    },
+    "WAS": {
+        "name": "FedExField",
+        "location": "Landover, Maryland",
+        "description": "Home of the Washington Commanders.",
+        "image": "./assets/WAS-In.jpg",
+        "other_teams": "N/A",
+        "capacity": 82000,
+        "year_opened": 1997,
+        "playing_surface": "Latitude 36 Bermuda Grass"
+    }
+}
